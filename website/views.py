b9255d06abeb510f93c7748713ae86b21e445141
DiogoLeiteF <98144351+RelevantSilence@users.noreply.github.com>
2022-08-03 14:27:45 +0100

    graphs added

--- website/views.py
+++ website/views.py
@@ -1,19 +1,29 @@
 from flask import Blueprint, flash, redirect, render_template, request, flash, jsonify, session, url_for
 from flask_login import login_required, current_user
 from website.dummy_data import add_dummy_data
-from .models import User, Product, Sale
+from .models import Sale_prod_list, User, Product, Sale
 from . import db
 import json
 from sqlalchemy import update, text
 import ast
+import matplotlib.pyplot as plt
+import os
 
 
 views = Blueprint('views', __name__)
 
 
+###########################
+# home
+###########################
+
 @views.route('/', methods=['GET', 'POST'])
 def home():
     products = Product.query.all()
+
+    # add dummy data to db
+    if len(products) < 1:
+        add_dummy_data(db=db, User=User, Sale=Sale, Product=Product)
 
     if current_user.is_authenticated:
         cart = ast.literal_eval(current_user.cart)
@@ -23,12 +33,6 @@ def home():
         session["cart"] = cart_len
     else:
         session["cart"] = 0
-
-    ###################################
-    ####     ADD DATA to DB      ######
-    ##################################
-    
-    # add_dummy_data(db=db, User=User, Sale=Sale, Product=Product)
 
     return render_template('home.html', user=current_user, products=products, cart_session=session["cart"])
 
@@ -49,14 +53,21 @@ def add_to_cart(user_id, prod_id):
 
     db.session.add(user)
     db.session.commit()
-    print(user.cart)
-    print(len(user.cart))
+    flash(f'{prod.name} added to cart')
     return redirect(url_for('views.home', ))
+
+
+###########################
+# User
+###########################
 
 
 @views.route('/user/<stat>')
 @login_required
 def user(stat='all'):
+    products = Product.query.all()
+    sales = Sale.query.filter_by(user_id=current_user.id).all()
+
     if stat == 'all':
         sales = Sale.query.filter_by(user_id=current_user.id).all()
     elif stat == 'awaits-payment':
@@ -75,7 +86,30 @@ def user(stat='all'):
         sales = Sale.query.filter_by(
             user_id=current_user.id, status='Done').all()
 
-    return render_template('user.html', user=current_user, sales=sales, cart_session=session["cart"])
+    print(os.listdir('./website/static/graphs'))
+    if len(os.listdir('./website/static/graphs')) > 0:
+        for file in os.listdir('./website/static/graphs'):
+            os.remove(f'./website/static/graphs/{file}')
+
+    print(os.listdir('./website/static/graphs'))
+
+    # graph 1 value over time
+    date = [sale.date_created.date() for sale in sales]
+    value = [sale.total_value for sale in sales]
+    # for sale in sales:
+    #     date.append(sale.date_created.date())
+    #     value.append(sale.total_value)
+    print(date)
+    print(value)
+    # plt.title('Value Spent')
+    # plt.xlabel('Date')
+    plt.ylabel('Value €')
+    plt.xticks(rotation=45)
+    plt.plot(date, value, color='red')
+    plt.tight_layout()
+    plt.savefig('./website/static/graphs/user1.png', dpi=300, format='png')
+
+    return render_template('user.html', user=current_user, sales=sales, cart_session=session["cart"], products=products)
 
 
 @views.route('/user/all/update-data', methods=['GET', 'POST'])
@@ -107,11 +141,64 @@ def update_user():
         return redirect(url_for('views.user', stat='all'))
 
 
+###########################
+# Supplier
+###########################
+
 @views.route('/supplier')
 @login_required
 def supplier():
-    return render_template('supplier.html', user=current_user, cart_session=session["cart"])
+    products = Product.query.filter_by(supplier=current_user.first_name).all()
+    sales = Sale.query.all()
 
+    # hist sold products
+    names = [x.name for x in products]
+    quant = [x.sold for x in products]
+    plt.title(' Products Sold')
+    # # plt.xlabel('Date')
+    plt.ylabel('Quantities')
+    # plt.xticks(rotation=45)
+    plt.bar(names, quant, color='grey')
+    plt.tight_layout()
+    plt.savefig('./website/static/graphs/supp1.png', dpi=300, format='png')
+    plt.close()
+
+    # Sales over time
+    time_ = []
+    value = 0
+    value_list = []
+    for sale in sales:
+        for prod in sale.prod_list:
+            for product in products:
+                if prod.product_id == product.prod_id:
+                    time_.append(sale.date_created.date())
+                    value += product.supplier_price
+                    value_list.append(value)
+
+    print(time_)
+    print(value)
+    print(value_list)
+    plt.title('Sales Over Time')
+    plt.xticks(rotation=45)
+    plt.hist(time_, color='grey')
+    plt.tight_layout()
+    plt.savefig('./website/static/graphs/supp2.png', dpi=300, format='png')
+    plt.close()
+
+    # income over time
+    plt.title('Cumulative Income')
+    plt.xticks(rotation=45)
+    plt.plot(time_, value_list, color='grey')
+    plt.tight_layout()
+    plt.savefig('./website/static/graphs/supp3.png', dpi=300, format='png')
+    plt.close()
+
+    return render_template('supplier.html', user=current_user, cart_session=session["cart"], products=products)
+
+
+###########################
+# Cart
+###########################
 
 @views.route('/cart/<id>', methods=['GET', 'POST'])
 @login_required
@@ -127,29 +214,39 @@ def cart(id):
 
     if request.method == 'POST':
         if user.cart != '{}':
-
+            # get info from order
             note = request.form.get('delivery-note')
             payment_method = request.form.get('payment_method')
+
+            # create new sale object
             new_sale = Sale(delivery_notes=note,
                             user_id=current_user.id, material=current_user. cart,
                             profit=0,
                             total_value=0,
                             payment_method=payment_method)
+
+            # Update values in new sale
             for prod, quant in cart.items():
-                for x in range(quant):
-                    new_sale.products.append(
-                        Product.query.filter_by(prod_id=prod).first())
-
-            print(new_sale.products)
-
-            for product in new_sale.products:
-                product.stock -= 1
-                product.sold += 1
-                new_sale.total_value += product.retail_price
-                new_sale.profit += product.retail_price - product.supplier_price
+                for product in products:
+                    if product.prod_id == prod:
+                        product.stock -= quant
+                        product.sold += quant
+                        new_sale.total_value += product.retail_price * quant
+                        new_sale.profit += product.retail_price * \
+                            quant - product.supplier_price * quant
 
             db.session.add(new_sale)
+            db.session.commit()
+            # print(new_sale.sale_id)
+            for prod, quant in cart.items():
+                for x in range(quant):
+                    new_sale_prod = Sale_prod_list(sale_id=new_sale.sale_id,
+                                                   user_id=user.id, product_id=prod)
+                    db.session.add(new_sale_prod)
+                    db.session.commit()
+
             user.cart = '{}'
+
             db.session.commit()
             flash('Your order has been created', category='success')
             return redirect(url_for('views.payment', id=new_sale.sale_id))
@@ -215,6 +312,10 @@ def cart_del(prod_id, id):
 
     return redirect(url_for('views.cart', id=user.id))
 
+
+###########################
+# Payment
+###########################
 
 @views.route('/payment/<id>')
 @login_required
